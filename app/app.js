ea4bc40ba7f3e342bc44203d3878695c583747d2
duydc22v7n503-debug <duydc22v7n503@vlvh.ctu.edu.vn>
2026-03-27 19:39:35 +0700

    tao tap tin app.js

---
+++ app/app.js
@@ -0,0 +1,22 @@
+const express   = require('express');
+const cors     = require('cors');
+const app      = express();
+
+app.use(cors());
+app.use(express.json());
+const contactRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactRouter);
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to contact book application.'});
+});
+const ApiError = require("./app/api-error");
+app.use("api/contacts", contactRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+}  );
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+module.exports = app;
