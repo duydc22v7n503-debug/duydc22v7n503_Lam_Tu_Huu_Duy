5b3699089e994883963441b3c564123ab89e7dcb
duydc22v7n503-debug <duydc22v7n503@vlvh.ctu.edu.vn>
2026-03-27 21:06:14 +0700

    them ham delete service

--- app/service/contact.service.js
+++ app/service/contact.service.js
@@ -58,6 +58,12 @@ class ContactService {
         );
         return result; // return result;
     }
+    async delete(id) {
+        const result = await this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+        return result;
+    }
 
 }
 
